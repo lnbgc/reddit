79dafefca203df60c48ac7cdf65c4cb48a79d372
lngbc <lena.bageac@epitech.eu>
2023-11-25 00:04:40 +0100

    Login/Register with email and password

--- src/components/auth/Signin.jsx
+++ src/components/auth/Signin.jsx
@@ -1,8 +1,58 @@
 import { Button } from "@components/ui/Button"
 import { Input } from "@components/ui/Input"
 import { Google } from "./Google"
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@utils/firebase";
+import { ROOT } from "@routes/routes";
 
 export const Signin = ({ closeModal }) => {
+
+    const navigate = useNavigate();
+
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = async () => {
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            setSuccess("Succesfully logged in! Redirecting...");
+            setError("");
+            if (closeModal) {
+                closeModal();
+            }
+            navigate(ROOT)
+        } catch (error) {
+            if (error.code) {
+                switch (error.code) {
+                    case "auth/invalid-email":
+                    case "auth/wrong-password":
+                    case "auth/invalid-login-credentials":
+                        setError("Invalid email or password.");
+                        break;
+                    case "auth/user-not-found":
+                        setError("User not found.");
+                        break;
+                    default:
+                        setError("Something went wrong. Please try again later.");
+                        console.error("Could not complete signin:", error);
+                        break;
+                }
+            } else {
+                console.error("Could not signin user:", error)
+            }
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        signIn();
+    }
+
     return (
         <div className="flex flex-col gap-6">
             <Google closeModal={closeModal} />
@@ -11,21 +61,30 @@ export const Signin = ({ closeModal }) => {
                 <span className="text-xs text-faint">OR</span>
                 <span className="h-px w-1/2 bg-faint" />
             </div>
-            <form className="space-y-2">
+            <form onSubmit={handleSubmit} className="space-y-2">
+                {error && <span className="error">{error}</span>}
+                {success && <span className="success">{success}</span>}
                 <Input
                     type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     label="Email address"
                     placeholder="cool.redditor@mail.com"
                 />
                 <Input
                     type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     label="Password"
                     placeholder="••••••••"
                 />
+                <div className="pt-4">
+                    <Button type="primary" width="full">
+                        Log In
+                    </Button>
+                </div>
             </form>
-            <Button type="primary">
-                Log In
-            </Button>
+
         </div>
     )
 }
--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,8 +1,94 @@
 import { Button } from "@components/ui/Button"
 import { Input } from "@components/ui/Input"
 import { Google } from "./Google"
+import { useAuth } from "@contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "@utils/firebase";
+import { ROOT } from "@routes/routes";
 
 export const Signup = ({ closeModal }) => {
+
+    const { usernameCheck, createUser } = useAuth();
+    const navigate = useNavigate();
+
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirm, setConfirm] = useState("");
+
+    const checkPassword = (password, confirm) => {
+        if (password !== confirm) {
+            setError("Passwords do not match.");
+            return false;
+        }
+        return true;
+    }
+
+    const checkUsername = (username) => {
+        if (username === "") {
+            setError("Username cannot be empty.");
+            return false;
+        }
+        if (username.length < 5 || username.length > 20) {
+            setError("Username must be between 5 and 20 characters.");
+            return false;
+        }
+        if (!/^[a-zA-Z0-9_]+$/.test(username)) {
+            setError("Only letters, numbers, and underscores allowed.");
+            return false;
+        }
+        return true;
+    }
+
+    const signUp = async (username, email, password) => {
+        try {
+            const isUsernameAvailable = await usernameCheck(username);
+            if (!isUsernameAvailable) {
+                setError("Username is already taken.");
+                return;
+            }
+            const { user } = await createUserWithEmailAndPassword(auth, email, password);
+            await updateProfile(user, { displayName: username });
+            await createUser(user.uid, username, email);
+            setSuccess("Successfully registered! Redirecting...");
+            setError("");
+            navigate(ROOT);
+        } catch (error) {
+            if (error.code) {
+                switch (error.code) {
+                    case "auth/invalid-email":
+                        setError("Invalid email address.")
+                        break;
+                    case "auth/weak-password":
+                        setError("Password must have at least 6 characters..")
+                        break;
+                    case "auth/email-already-in-use":
+                        setError("Account already registered with this email address.")
+                        break;
+                    default:
+                        setError("Something went wrong. Please try again later.")
+                        console.error("Could not complete signup:", error);
+                        break;
+                }
+            } else {
+                console.error("Could not signup user:", error)
+            }
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (checkPassword(password, confirm) && checkUsername(username)) {
+            signUp(username, email, password);
+        }
+    }
+
+
     return (
         <div className="flex flex-col gap-6">
             <Google closeModal={closeModal} />
@@ -11,31 +97,43 @@ export const Signup = ({ closeModal }) => {
                 <span className="text-xs text-faint">OR</span>
                 <span className="h-px w-1/2 bg-faint" />
             </div>
-            <form className="space-y-2">
+            <form onSubmit={handleSubmit} className="space-y-2">
+                {error && <span className="error">{error}</span>}
+                {success && <span className="success">{success}</span>}
                 <Input
                     type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
                     label="Username"
                     placeholder="CoolRedditor"
                 />
                 <Input
                     type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     label="Email address"
                     placeholder="cool.redditor@mail.com"
                 />
                 <Input
                     type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     label="Password"
                     placeholder="••••••••"
                 />
                 <Input
                     type="password"
+                    value={confirm}
+                    onChange={(e) => setConfirm(e.target.value)}
                     label="Confirm password"
                     placeholder="••••••••"
                 />
+                <div className="pt-4">
+                    <Button type="primary" width="full">
+                        Register
+                    </Button>
+                </div>
             </form>
-            <Button type="primary">
-                Register
-            </Button>
         </div>
     )
 }
