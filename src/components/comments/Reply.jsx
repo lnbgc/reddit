3f218566a36d3bf825efdee6b5c0896eeb27e375
lngbc <lena.bageac@epitech.eu>
2023-12-01 00:36:44 +0100

    Render and looping replies

--- src/components/comments/Reply.jsx
+++ src/components/comments/Reply.jsx
@@ -1,49 +1,47 @@
-import { useEffect, useState } from "react";
 import { db } from "@utils/firebase";
 import { collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { CreateReply } from "./CreateReply";
-import { Reply } from "./Reply";
 
-export const Comment = ({ comment, postAuthor, postID }) => {
+export const Reply = ({ reply, postID, postAuthor }) => {
     const [creatorData, setCreatorData] = useState(null);
     const [replies, setReplies] = useState([]);
 
-    const fetchCommentData = async () => {
+    const fetchReplyData = async () => {
         try {
-            const userDoc = await getDoc(doc(db, "users", comment.createdBy));
+            const userDoc = await getDoc(doc(db, "users", reply.createdBy));
             if (userDoc.exists()) {
                 setCreatorData(userDoc.data());
             }
         } catch (error) {
-            console.error("Could not fetch comment data:", error);
+            console.error("Could not fetch reply data:", error);
         }
-    };
+    }
 
     const fetchReplies = async () => {
         try {
             const replies = collection(db, "replies");
-            const q = query(replies, where("parent_id", "==", comment.id));
+            const q = query(replies, where("parent_id", "==", reply.id));
             const querySnapshot = await getDocs(q);
             const repliesData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
             setReplies(repliesData);
         } catch (error) {
-            console.error("Could not fetch replies:", error);
+            console.error("Could not fetch replies to replies:", error);
         }
     }
 
     useEffect(() => {
-        fetchCommentData();
+        fetchReplyData();
         fetchReplies();
-    }, [comment.createdBy, comment.id]);
-
+    }, [reply.createdBy, reply.id]);
     return (
         <div className="flex flex-col gap-4">
             {creatorData && (
                 <>
                     <div className="flex items-center">
                         <div className="flex items-center gap-2 text-sm font-medium">
-                            <img src={creatorData.avatar} alt="" className="avatar-sm" />
-                            {comment.createdBy === postAuthor ? (
+                            <img src={creatorData.avatar} className="avatar-sm" />
+                            {reply.createdBy === postAuthor ? (
                                 <div className="space-x-1">
                                     <span className="font-bold">OP</span>
                                     <span>u/{creatorData.username}</span>
@@ -57,9 +55,9 @@ export const Comment = ({ comment, postAuthor, postID }) => {
                     </div>
                     <div className="flex border-l-1.5 border-border flex-col gap-4 ml-3 pl-[18px]">
                         <p className="text-sm">
-                            {comment.content}
+                            {reply.content}
                         </p>
-                        <CreateReply commentID={comment.id} postID={postID} />
+                        <CreateReply commentID={reply.id} postID={postID} />
                         {replies.map((reply) => (
                             <Reply key={reply.id} reply={reply} postID={postID} postAuthor={postAuthor} />
                         ))}
